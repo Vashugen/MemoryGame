b9888a154dc60a9e4ccfc0dfa2dbbe93119eaee5
VashuGen <vashu_gen@mail.ru>
2021-11-06 12:41:30 +0300

    add MenuFragment

--- app/src/main/java/games/shmugen/memorygame/engine/ScreenController.java
+++ app/src/main/java/games/shmugen/memorygame/engine/ScreenController.java
@@ -1,8 +1,27 @@
 package games.shmugen.memorygame.engine;
 
+import android.widget.LinearLayout;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import games.shmugen.memorygame.common.Shared;
+
 public class ScreenController {
 
     private static ScreenController mInstance = null;
+    private FragmentManager mFragmentManager;
+    private static List<Screen> openedScreens = new ArrayList<>();
+
+    public static enum Screen {
+        MENU,
+        GAME,
+        DIFFICULTY,
+        THEME_SELECT
+    }
 
     public static ScreenController getInstance(){
         if(mInstance == null){
@@ -10,6 +29,28 @@ public class ScreenController {
         }
 
         return mInstance;
+    }
+
+    public void openScreen(Screen screen){
+        mFragmentManager = Shared.activity.getSupportFragmentManager();
+
+        if(screen == Screen.GAME && openedScreens.get(openedScreens.size() - 1) == Screen.GAME){
+            openedScreens.remove(openedScreens.size() - 1);
+        }else if (screen == Screen.DIFFICULTY && openedScreens.get(openedScreens.size() - 1) == Screen.GAME) {
+            openedScreens.remove(openedScreens.size() - 1);
+            openedScreens.remove(openedScreens.size() - 1);
+        }
+
+        Fragment fragment = getFragment(screen);
+    }
+
+    private Fragment getFragment(Screen screen){
+        switch (screen){
+            case MENU:
+                //return new Men
+                break;
+        }
+        return new Fragment();
     }
 
 }
---
+++ app/src/main/java/games/shmugen/memorygame/fragments/MenuFragment.java
@@ -0,0 +1,18 @@
+package games.shmugen.memorygame.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MenuFragment extends Fragment {
+
+    /*@Override
+    public View onCreateView(LayoutInflater inFlater, ViewGroup container, Bundle savedInstanceState) {
+
+    }*/
+}
