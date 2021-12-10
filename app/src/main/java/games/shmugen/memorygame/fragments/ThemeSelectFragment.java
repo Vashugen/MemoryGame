5243482053123929d90835dcfe287a2b1d9136a8
Marina Mironova <vashu_gen@mail.ru>
2021-12-10 19:00:55 +0300

    add setStars ThemeSelectFragment

--- app/src/main/java/games/shmugen/memorygame/fragments/ThemeSelectFragment.java
+++ app/src/main/java/games/shmugen/memorygame/fragments/ThemeSelectFragment.java
@@ -9,7 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import java.util.Locale;
+
 import games.shmugen.memorygame.R;
+import games.shmugen.memorygame.common.Memory;
 import games.shmugen.memorygame.common.Shared;
 import games.shmugen.memorygame.themes.Theme;
 import games.shmugen.memorygame.themes.Themes;
@@ -32,11 +35,18 @@ public class ThemeSelectFragment extends Fragment {
         return inflater.inflate(R.layout.theme_select_fragment, container, false);
     }
 
-    private void setStars(ImageView imageView, Theme theme, String type){
+    private void setStars(ImageView imageView, Theme theme, String type) {
         int sum = 0;
 
-        for (int difficulty = 1; difficulty <= 6; difficulty++){
+        for (int difficulty = 1; difficulty <= 6; difficulty++) {
+            sum += Memory.getHighClass(theme.id, difficulty);
+        }
 
+        int num = sum / 6;
+        if (num != 0) {
+            String drawableResourceName = String.format(Locale.US, type + "_theme_star_%d", num);
+            int drawableResourceId = Shared.context.getResources().getIdentifier(drawableResourceName, "drawable", Shared.context.getPackageName());
+            imageView.setImageResource(drawableResourceId);
         }
     }
 }
